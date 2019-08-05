685d28242ee80b516629966e8445a5305400db51
saisanjcharlis <charlis@saisanj.com>
2019-08-06 01:14:25 +0530

    Passbook and Transactions

--- src/app/passbook/passbook.component.ts
+++ src/app/passbook/passbook.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $:any;
 @Component({
   selector: 'app-passbook',
   templateUrl: './passbook.component.html',
   styleUrls: ['./passbook.component.css']
 })
 export class PassbookComponent implements OnInit {
-
+  plots;
   constructor() { }
-
+  addPassbook(){
+    $('.ui.modal').modal('show');
+  }
+  allotPassbook(){
+    $('body').toast({
+      class: 'success',
+      message: `Your passbook has been alloted`
+    });
+  }
   ngOnInit() {
+
+    this.plots =  ['Windstorm', 'Bombasto','asdas'];
   }
 
 }
--- src/app/transactions/transactions.component.ts
+++ src/app/transactions/transactions.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $:any;
 @Component({
   selector: 'app-transactions',
   templateUrl: './transactions.component.html',
@@ -10,6 +10,23 @@ export class TransactionsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $('.pop').popup({
+      inline     : true,
+      hoverable  : true,
+      position   : 'bottom left',
+      delay: {
+        show: 300,
+        hide: 800
+      }
+    });
+    $('#example').DataTable( {
+      columnDefs: [
+          {
+              targets: [ 0, 1, 2 ],
+              className: 'mdl-data-table__cell--non-numeric'
+          }
+      ]
+  } );
   }
 
 }
