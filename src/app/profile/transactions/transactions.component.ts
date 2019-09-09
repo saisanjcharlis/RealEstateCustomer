0bd025e7e96e9f872086903afb6fb7fb9cd2b9c8
saisanjcharlis <charlis@saisanj.com>
2019-09-10 00:47:37 +0530

    Transactions Done

--- src/app/profile/transactions/transactions.component.ts
+++ src/app/profile/transactions/transactions.component.ts
@@ -8,32 +8,7 @@ declare var $:any;
 export class TransactionsComponent implements OnInit {
 
   constructor() { }
-  onHide(hide: boolean){
-    if(hide){
-      $('.gridContainer').css( "grid-template-columns", "15% 80%" );
-    } else {
-      $('.gridContainer').css( "grid-template-columns", "4% 90%" );
-    }
-
-  }
   ngOnInit() {
-    $('.pop').popup({
-      inline     : true,
-      hoverable  : true,
-      position   : 'bottom left',
-      delay: {
-        show: 300,
-        hide: 800
-      }
-    });
-    $('#example').DataTable( {
-      columnDefs: [
-          {
-              targets: [ 0, 1, 2 ],
-              className: 'mdl-data-table__cell--non-numeric'
-          }
-      ]
-  } );
   }
 
 }
